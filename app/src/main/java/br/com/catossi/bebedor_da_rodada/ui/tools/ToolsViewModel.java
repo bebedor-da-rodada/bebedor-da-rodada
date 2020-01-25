bc1b135f62f1e4508247540875df4a62cb207e18
Matheus Catossi <mcatossi@br.ibm.com>
2020-01-25 14:30:47 -0300

    first commit

---
+++ app/src/main/java/br/com/catossi/bebedor_da_rodada/ui/tools/ToolsViewModel.java
@@ -0,0 +1,19 @@
+package br.com.catossi.bebedor_da_rodada.ui.tools;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ToolsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ToolsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is tools fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
